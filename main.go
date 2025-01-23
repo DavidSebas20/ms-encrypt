f5aa1b2c23c7177be575a086bb17e0e8791742bc
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-22 20:12:45 -0500

    :sparkles: feat($main.go): add request and response structs

--- main.go
+++ main.go
@@ -5,6 +5,25 @@ import (
 	"net/http"
 )
 
+// Hash struct
+type HashRequest struct {
+	Password string `json:"password"`
+}
+
+type HashResponse struct {
+	Hash string `json:"hash"`
+}
+
+// Verify struct
+type VerifyRequest struct {
+	Password string `json:"password"`
+	Hash     string `json:"hash"`
+}
+
+type VerifyResponse struct {
+	Match bool `json:"valid"`
+}
+
 func main() {
 	log.Println("Microservice running on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
