f1498b331be7dfd262400868a71633ec79331f48
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-22 20:20:05 -0500

    :sparkles: feat($main.go): implement password hashing function and endpoint

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Hash struct
@@ -24,7 +27,28 @@ type VerifyResponse struct {
 	Match bool `json:"valid"`
 }
 
+// HashPassword using bcrypt
+func HashPassword(w http.ResponseWriter, r *http.Request) {
+	var req HashRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		return
+	}
+
+	res := HashResponse{Hash: string(hash)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
 func main() {
+	http.HandleFunc("/hash", HashPassword)
+
 	log.Println("Microservice running on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
